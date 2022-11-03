4741a8f322b9836b1468188fbcec3e2b78a2644a
Artem Bilas <artembilas@gmail.com>
2022-11-03 12:25:05 +0200

    feat: add filter component

---
+++ src/componets/Filter-Item/Filter-Item.jsx
@@ -0,0 +1,11 @@
+import './Filter-Item.scss';
+
+const FilterItem = () => {
+  return <div className='filter-block'>
+    <span className=''>All</span>
+    <span>Complete</span>
+    <span>Todo</span>
+  </div>;
+};
+
+export default FilterItem;
--- src/componets/TodoList/TodoList.jsx
+++ src/componets/TodoList/TodoList.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import {useSelector} from "react-redux";
-import {Empty} from "antd";
+import { useSelector } from "react-redux";
+import { Empty } from "antd";
 import TodoItem from "../TodoItem/TodoItem";
 import './TodoList.scss';
+import FilterItem from '../Filter-Item/Filter-Item';
 
 const TodoList = () => {
-    const {todos} = useSelector(state => state.todos);
+    const { todos } = useSelector(state => state.todos);
 
     return <div className='todo-list'>
         {todos.length
-            ? todos.map(item => <TodoItem key={item.id} {...item} />)
-            : <><Empty description={false}/></>
+            ? (
+                <>
+                    <FilterItem />
+                    {todos.map(item => <TodoItem key={item.id} {...item} />)}
+                </>
+            ) : (<><Empty description={false} /></>)
         }
     </div>
 }
